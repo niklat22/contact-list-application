2f07f68c35297b537188cd074466f57766ef6e3b
niklat22 <lathiyanikund@gmail.com>
2022-01-06 23:14:20 +0530

    create contact app

---
+++ src/app/ContactList/contact-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output,
+   OnChanges, DoCheck, SimpleChanges } from '@angular/core';
+import { ContactDataService } from '../ContactData.service';
+
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-list-component',
+  templateUrl: './contact-list.component.html'
+})
+export class ContactListComponent implements OnInit {
+
+    contactList: Contact[] = [];
+
+  contactServObj: ContactDataService;
+
+
+  constructor(contactServObj: ContactDataService) {
+    this.contactServObj = contactServObj;
+  }
+
+
+  ngOnInit() {
+    this.contactList = this.contactServObj.getContacts();
+    // console.log('ngOnInit called!');
+  }
+
+  fetchUpdatedList(){
+    this.contactList = this.contactServObj.getContacts(); 
+  }
+}
